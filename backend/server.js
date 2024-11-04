b747621e54e21b0cf2e79ad0d72954dbc233f899
Julian Vargas <julianvargastrb@gmail.com>
2024-11-03 21:56:45 -0500

    server

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const connectDB = require('./db');
 const cors = require('cors');
 const routes = require('./routes');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,8 +15,16 @@ connectDB();
 app.use(cors());
 app.use(express.json()); // Para parsear JSON
 
-// Rutas
+// Rutas de la API
 app.use('/api', routes);
+
+// Middleware para servir archivos estáticos (frontend)
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Ruta para la raíz (opcional)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html')); // Sirve el archivo index.html
+});
 
 // Iniciar el servidor
 app.listen(PORT, () => {
